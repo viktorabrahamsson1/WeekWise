2f7b0cda6ee3118da461980762514a234c480a67
Viktor Abrahamsson <vikabr0524@gmail.com>
2024-03-20 21:22:34 +0100

    changed cors url

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://week-wise.vercel.app",
     credentials: true,
   })
 );
