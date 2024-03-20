c8611bcf236d662a40fbf7989a41b7b050aa1c9d
Viktor Abrahamsson <vikabr0524@gmail.com>
2024-03-20 21:31:29 +0100

    added vercel file in client

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://week-wise.vercel.app",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
