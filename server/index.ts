a6432971c1fbad6fa71915229fcef01178854d33
Viktor Abrahamsson <vikabr0524@gmail.com>
2024-03-21 10:51:47 +0100

    added cors again

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,12 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => res.send("Server is working"));
 
 app.use("/api/auth", authRoutes);
