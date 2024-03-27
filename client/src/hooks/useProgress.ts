955049ae7cf8879731df2ced86a4239f376723c9
Viktor Abrahamsson <vikabr0524@gmail.com>
2024-03-27 21:56:46 +0100

    changed an un used parameter

--- client/src/hooks/useProgress.ts
+++ client/src/hooks/useProgress.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const useProgress = () => {
@@ -16,13 +17,42 @@ const useProgress = () => {
       );
       const body = await response.json();
       const { allTasks, completedTasks } = body;
-      setAllTaskss(allTasks);
-      setAllCompeltedTasks(completedTasks);
+      //setAllTaskss(allTasks);
+      //setAllCompeltedTasks(completedTasks);
       if (!response.ok) throw new Error(body.message);
     } catch (error) {
       console.error("Error fetching progress data:", error);
     }
   };
+
+  const testprogress = async () => {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/api/calenderTask/getProgress`,
+        {
+          method: "POST",
+          credentials: "include",
+        },
+      );
+      const { allTasks, completedTasks } = await response.json();
+
+      if (!response.ok) throw new Error("error");
+      return { allTasks, completedTasks };
+    } catch (error) {
+      throw new Error("error");
+    }
+  };
+
+  const { data } = useQuery({
+    queryFn: async () => testprogress(),
+    queryKey: "progress",
+    onSuccess: () => {
+      console.log(data);
+      if (!data) return;
+      setAllTaskss(data.allTasks);
+      setAllCompeltedTasks(data.completedTasks);
+    },
+  });
 
   fetchProgressData();
 
